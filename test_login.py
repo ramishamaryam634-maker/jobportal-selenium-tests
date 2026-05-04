13aa168456534114a752a8a2bf6a1ed8e1fc4057
Ramisha Maryam <macbook@Ramishas-MacBook-Pro.local>
2026-05-04 18:41:57 +0500

    fix auth routes + selenium selectors + waits

--- test_login.py
+++ test_login.py
@@ -2,26 +2,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
-def test_login_page_load(driver):
-    driver.get("http://13.48.6.111:3000/login")
-    assert "login" in driver.current_url.lower()
-
-
 def test_invalid_login(driver):
-    driver.get("http://13.48.6.111:3000/login")
+    driver.get("http://13.48.6.111:3000/auth/login")
 
-    email = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "email"))
-    )
+    wait = WebDriverWait(driver, 10)
+
+    email = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='email'], input#email")))
+    password = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='password'], input#password")))
+
     email.send_keys("wrong@test.com")
-
-    password = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "password"))
-    )
     password.send_keys("wrongpass")
 
-    button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, "button"))
-    )
-    button.click()
+    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button"))).click()
--- test_signup.py
+++ test_signup.py
@@ -2,31 +2,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
-def test_signup_page(driver):
-    driver.get("http://13.48.6.111:3000/signup")
-    assert "signup" in driver.current_url.lower()
-
-
 def test_signup_form(driver):
-    driver.get("http://13.48.6.111:3000/signup")
+    driver.get("http://13.48.6.111:3000/auth/register")
 
-    name = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "name"))
-    )
+    wait = WebDriverWait(driver, 10)
+
+    name = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='name'], input#name")))
+    email = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='email'], input#email")))
+    password = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='password'], input#password")))
+
     name.send_keys("Test User")
-
-    email = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "email"))
-    )
     email.send_keys("test@test.com")
-
-    password = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "password"))
-    )
     password.send_keys("123456")
 
-    button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, "button"))
-    )
-    button.click()
+    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button"))).click()
