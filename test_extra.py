99ce96ec5a9ec8449e9ca5c9e87a24142acda278
ramishamaryam634-maker <ramishamaryam634@gmail.com>
2026-05-04 19:38:58 +0500

    Update test_extra.py

--- test_extra.py
+++ test_extra.py
@@ -1,13 +1,24 @@
+from selenium.webdriver.common.by import By
+
 def test_dummy_1(driver):
     driver.get("http://13.48.6.111:3000")
     assert True
-
 
 def test_dummy_2(driver):
     driver.get("http://13.48.6.111:3000")
     assert "Job" in driver.title
 
-
 def test_dummy_3(driver):
     driver.get("http://13.48.6.111:3000")
     assert True
+
+
+def test_page_has_content(driver):
+    driver.get("http://13.48.6.111:3000")
+    assert len(driver.page_source) > 100
+
+
+def test_page_has_body_tag(driver):
+    driver.get("http://13.48.6.111:3000")
+    body = driver.find_element(By.TAG_NAME, "body")
+    assert body is not None
