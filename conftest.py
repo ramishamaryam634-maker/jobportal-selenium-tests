5bf54a8aa650701fbbf78aac83c6720ecf8048f7
ramishamaryam634-maker <ramishamaryam634@gmail.com>
2026-05-04 16:21:12 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -5,9 +5,11 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture
 def driver():
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=options)
     yield driver
