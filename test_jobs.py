3253a242399d07609249a85a3d41fed2cb3a8872
ramishamaryam634-maker <ramishamaryam634@gmail.com>
2026-05-04 19:40:26 +0500

    Update test_jobs.py

--- test_jobs.py
+++ test_jobs.py
@@ -4,7 +4,6 @@ def test_jobs_page(driver):
     driver.get("http://13.48.6.111:3000/jobs")
     assert "job" in driver.current_url.lower()
 
-
 def test_job_list_visible(driver):
     driver.get("http://13.48.6.111:3000/jobs")
-    assert True  # basic page load test
+    assert True
